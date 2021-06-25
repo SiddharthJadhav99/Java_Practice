b64d8710bd546b25a15683b483425579756899af
Siddharth Jadhav <jadhavsid99@gmail.com>
2021-06-25 23:14:14 +0530

    Changes in main

--- newproject-/src/com/company/Conditional_statement.java
+++ newproject-/src/com/company/Conditional_statement.java
@@ -1,5 +1,5 @@
 package com.company;
-import java.sql.SQLOutput;
+//import java.sql.SQLOutput;
 import java.util.Scanner;
 public class Conditional_statement {
     public static void main(String[] args){
